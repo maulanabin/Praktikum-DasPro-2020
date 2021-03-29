b22bb7d23f72aca25b9c11ff3f3f4e6f7ac53f86
Maulana Bintang Irfansyah <77730390+maulanabin@users.noreply.github.com>
2021-03-29 16:53:33 +0700

    Add files via upload

---
+++ Pertemuan 1/src/biodataDiri.java
@@ -0,0 +1,20 @@
+public class biodataDiri {
+    public static void main(String[] args) {
+        String namaMhs = "Maulana Bintang Irfansyah";
+        String nimMhs = "2041720132";
+        String prodiMhs = "D4 Teknik Informatika";
+        String alamatMhs = "Perum Pondok Pabean Indah, Probolinggo, Jawa Timur";
+        String noHp = "081938480203";
+        String hobiMhs = "Memasak dan Berkebun";
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("\t\tBiodata Mahasiswa Jurusan Teknologi Informasi");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Nama\t: " + namaMhs);
+        System.out.println("NIM\t\t: " + nimMhs);
+        System.out.println("Jurusan\t: " + prodiMhs);
+        System.out.println("Alamat\t: " + alamatMhs);
+        System.out.println("No HP\t: " + noHp);
+        System.out.println("Hobi\t: " + hobiMhs);
+    }
+}
